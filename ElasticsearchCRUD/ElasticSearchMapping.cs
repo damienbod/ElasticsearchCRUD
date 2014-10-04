8b36807464c4dca2998ef377e0635301e30f802e
damienbod <damien_bod@hotmail.com>
2014-10-04 10:35:59 +0200

    first refactoring, does not run

--- ElasticsearchCRUD/ElasticSearchMapping.cs
+++ ElasticsearchCRUD/ElasticSearchMapping.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
 using Newtonsoft.Json;
 
 namespace ElasticsearchCRUD
@@ -17,10 +19,55 @@ namespace ElasticsearchCRUD
 			var propertyInfo = entity.GetType().GetProperties();
 			foreach (var prop in propertyInfo)
 			{
-				MapValue(prop.Name.ToLower(), prop.GetValue(entity));
+				if (IsPropertyACollection(prop))
+				{
+					// TODO test for object type or simple type
+					MapSimpleArrayValue(prop);
+
+					// TODO
+					//if (object.IsElasticsearchDocumentType)
+					//{
+					//	// add to bulk insert as own index
+					//}
+					//else
+					//{
+					//	MapObjectArrayValue(prop);
+					//}
+				}
+				else
+				{
+					MapValue(prop.Name.ToLower(), prop.GetValue(entity));
+				}	
 			}
 		}
 
+		// Nested
+		// "tags" : ["elasticsearch", "wow"], (string array or int array)
+		protected void MapSimpleArrayValue(PropertyInfo prop)
+		{
+			Writer.WriteEndObject();
+			//Writer.WritePropertyName("data:");
+			Writer.WriteStartArray();
+
+			for (int t = 0; t < 1; t++)
+			{
+				
+			}
+
+			Writer.WriteEndArray();
+			Writer.WriteStartObject();
+		}
+
+		// Nested
+		//"lists" : [
+		//	{
+		//		"name" : "prog_list",
+		//		"description" : "programming list"
+		//	},
+		protected void MapObjectArrayValue(PropertyInfo prop)
+		{
+		}
+		
 		public void AddWriter(JsonWriter writer)
 		{
 			Writer = writer;
@@ -31,6 +78,15 @@ namespace ElasticsearchCRUD
 			Writer.WritePropertyName(key);
 			Writer.WriteValue(valueObj);
 		}
+
+		public bool IsPropertyACollection(PropertyInfo property)
+		{
+			if (property.PropertyType.FullName == "System.String")
+			{
+				return false;
+			}
+			return property.PropertyType.GetInterface(typeof(IEnumerable<>).FullName) != null;
+		} 
 
 		public virtual object ParseEntity(Newtonsoft.Json.Linq.JToken source, Type type)
 		{
