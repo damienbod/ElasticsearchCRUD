72548e0317e7762073be14f8654d349f39b0d306
damienbod <damien_bod@hotmail.com>
2014-10-28 22:53:40 +0100

    code clean up

--- ElasticsearchCRUD/ContextSearch/ElasticsearchContextSearch.cs
+++ ElasticsearchCRUD/ContextSearch/ElasticsearchContextSearch.cs
@@ -74,7 +74,7 @@ namespace ElasticsearchCRUD.ContextSearch
 			
 			var search = new Search(_traceProvider, _cancellationTokenSource, _elasticsearchSerializerConfiguration, _client, _connectionString);
 
-			var result = await search.PostSearchAsync<T>(BuildSearchById<T>(entityId));
+			var result = await search.PostSearchAsync<T>(BuildSearchById(entityId));
 			resultDetails.TotalHits = result.TotalHits;
 			resultDetails.RequestBody = result.RequestBody;
 			resultDetails.RequestUrl = result.RequestUrl;
@@ -108,7 +108,7 @@ namespace ElasticsearchCRUD.ContextSearch
 		//	   }
 		//   }
 		// }
-		private string BuildSearchById<T>(object childId)
+		private string BuildSearchById(object childId)
 		{
 			var buildJson = new StringBuilder();
 			buildJson.AppendLine("{");
