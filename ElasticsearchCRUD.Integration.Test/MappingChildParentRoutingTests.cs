3e320bd50622365f31b664f2376270aff5e36b93
damienbod <damien_bod@hotmail.com>
2014-11-22 15:17:24 +0100

    adding test for mappings list

--- ElasticsearchCRUD.Integration.Test/MappingChildParentRoutingTests.cs
+++ ElasticsearchCRUD.Integration.Test/MappingChildParentRoutingTests.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Net;
 using System.Threading;
 using ElasticsearchCRUD.ContextAddDeleteUpdate;
@@ -40,12 +41,41 @@ namespace ElasticsearchCRUD.Integration.Test
 				var doc2 = context.GetDocument<MappingChildParentRoutingTestsLevel2>(2, new RoutingDefinition { ParentId = 1, RoutingId = 1 });
 				Assert.IsNotNull(doc2);
 
+				var doc3 = context.GetDocument<MappingChildParentRoutingTestsLevel3>(3, new RoutingDefinition { ParentId = 2, RoutingId = 1 });
+				Assert.IsNotNull(doc3);
+
 				context.AllowDeleteForIndex = true;
-				var result = context.DeleteIndexType<MappingChildParentRoutingTestsLevel2>();
+				var result = context.DeleteIndexType<MappingChildParentRoutingTestsLevel3>();
 				Assert.IsTrue(result);
 
 				Thread.Sleep(1000);
-				result = context.DeleteIndexType<MappingChildParentRoutingTestsLevel2>();
+				result = context.DeleteIndexType<MappingChildParentRoutingTestsLevel3>();
+				Assert.IsFalse(result);
+			}
+		}
+
+		[Test]
+		public void DeleteNonExistingChildTypeFromExistingIndexWithList()
+		{
+			CreateIndexList();
+
+			using (var context = new ElasticsearchContext(ConnectionString, _elasticsearchMappingResolver))
+			{
+				var doc1 = context.GetDocument<ListMappingChildParentRoutingTestsLevel1>(1);
+				Assert.IsNotNull(doc1);
+
+				var doc2 = context.GetDocument<ListMappingChildParentRoutingTestsLevel2>(2, new RoutingDefinition { ParentId = 1, RoutingId = 1 });
+				Assert.IsNotNull(doc2);
+
+				var doc3 = context.GetDocument<ListMappingChildParentRoutingTestsLevel3>(3, new RoutingDefinition { ParentId = 2, RoutingId = 1 });
+				Assert.IsNotNull(doc3);
+
+				context.AllowDeleteForIndex = true;
+				var result = context.DeleteIndexType<ListMappingChildParentRoutingTestsLevel3>();
+				Assert.IsTrue(result);
+
+				Thread.Sleep(1000);
+				result = context.DeleteIndexType<ListMappingChildParentRoutingTestsLevel3>();
 				Assert.IsFalse(result);
 			}
 		}
@@ -83,6 +113,46 @@ namespace ElasticsearchCRUD.Integration.Test
 				Assert.AreEqual(ret.Status, HttpStatusCode.OK);
 			}
 		}
+
+		private void CreateIndexList()
+		{
+			var doc = new ListMappingChildParentRoutingTestsLevel1
+			{
+				MappingChildParentRoutingTestsLevel1Id = 1,
+				Level2 = new List<ListMappingChildParentRoutingTestsLevel2>
+				{
+					new ListMappingChildParentRoutingTestsLevel2
+					{
+						MappingChildParentRoutingTestsLevel2Id = 2,
+						Level3 = new List<ListMappingChildParentRoutingTestsLevel3>
+					    {
+							new ListMappingChildParentRoutingTestsLevel3
+							{
+								MappingChildParentRoutingTestsLevel3Id = 3
+							}
+						}
+					}
+				}
+			};
+
+			_elasticsearchMappingResolver.AddElasticSearchMappingForEntityType(typeof(ListMappingChildParentRoutingTestsLevel1),
+			MappingUtils.GetElasticsearchMapping("masterindexlist"));
+			_elasticsearchMappingResolver.AddElasticSearchMappingForEntityType(typeof(ListMappingChildParentRoutingTestsLevel2),
+				MappingUtils.GetElasticsearchMapping("masterindexlist"));
+			_elasticsearchMappingResolver.AddElasticSearchMappingForEntityType(typeof(ListMappingChildParentRoutingTestsLevel3),
+				MappingUtils.GetElasticsearchMapping("masterindexlist"));
+
+			using (var context = new ElasticsearchContext(ConnectionString,
+					new ElasticsearchSerializerConfiguration(_elasticsearchMappingResolver, true, true, true)))
+			{
+				context.TraceProvider = new ConsoleTraceProvider();
+				context.AddUpdateDocument(doc, doc.MappingChildParentRoutingTestsLevel1Id);
+
+				var ret = context.SaveChangesAndInitMappings();
+				// Save to Elasticsearch
+				Assert.AreEqual(ret.Status, HttpStatusCode.OK);
+			}
+		}
 	}
 
 	public class MappingChildParentRoutingTestsLevel1
@@ -101,6 +171,27 @@ namespace ElasticsearchCRUD.Integration.Test
 	}
 
 	public class MappingChildParentRoutingTestsLevel3
+	{
+		[Key]
+		public short MappingChildParentRoutingTestsLevel3Id { get; set; }
+	}
+
+	public class ListMappingChildParentRoutingTestsLevel1
+	{
+		public short MappingChildParentRoutingTestsLevel1Id { get; set; }
+
+		public List<ListMappingChildParentRoutingTestsLevel2> Level2 { get; set; }
+	}
+
+	public class ListMappingChildParentRoutingTestsLevel2
+	{
+		[Key]
+		public short MappingChildParentRoutingTestsLevel2Id { get; set; }
+
+		public List<ListMappingChildParentRoutingTestsLevel3> Level3 { get; set; }
+	}
+
+	public class ListMappingChildParentRoutingTestsLevel3
 	{
 		[Key]
 		public short MappingChildParentRoutingTestsLevel3Id { get; set; }
