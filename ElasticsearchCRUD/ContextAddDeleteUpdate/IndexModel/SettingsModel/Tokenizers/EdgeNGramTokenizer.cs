623a068c6c8d392439b47c61291468d48a016635
damienbod <damien_bod@hotmail.com>
2014-12-05 13:56:18 +0100

    Adding NGramTokenizer

--- ElasticsearchCRUD/ContextAddDeleteUpdate/IndexModel/SettingsModel/Tokenizers/EdgeNGramTokenizer.cs
+++ ElasticsearchCRUD/ContextAddDeleteUpdate/IndexModel/SettingsModel/Tokenizers/EdgeNGramTokenizer.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using ElasticsearchCRUD.Model;
-using ElasticsearchCRUD.Utils;
+﻿using ElasticsearchCRUD.Model;
 
 namespace ElasticsearchCRUD.ContextAddDeleteUpdate.IndexModel.SettingsModel.Tokenizers
 {
@@ -16,106 +14,6 @@ namespace ElasticsearchCRUD.ContextAddDeleteUpdate.IndexModel.SettingsModel.Toke
 			AnalyzerSet = true;
 			Name = name.ToLower();
 			Type = DefaultTokenizers.EdgeNGram;
-		}
-	}
-
-	public class NGramTokenizer : BaseNGramTokenizer
-	{
-		/// <summary>
-		/// A tokenizer of type nGram.
-		/// </summary>
-		/// <param name="name">name of the custom tokenizer (ToLower()</param>
-		public NGramTokenizer(string name)
-		{
-			AnalyzerSet = true;
-			Name = name.ToLower();
-			Type = DefaultTokenizers.NGram;
-		}
-	}
-
-	public abstract class BaseNGramTokenizer : AnalysisTokenizerBase
-	{
-		protected int _minGram;
-		protected bool _minGramSet;
-		protected int _maxGram;
-		protected bool _maxGramSet;
-		protected List<TokenChar> _tokenChars;
-		protected bool _tokenCharsSet;
-
-		/// <summary>
-		/// min_gram Minimum size in codepoints of a single n-gram
-		/// </summary>
-		public int MinGram
-		{
-			get { return _minGram; }
-			set
-			{
-				_minGram = value;
-				_minGramSet = true;
-			}
-		}
-
-		/// <summary>
-		///  max_gram Maximum size in codepoints of a single n-gram
-		/// </summary>
-		public int MaxGram
-		{
-			get { return _maxGram; }
-			set
-			{
-				_maxGram = value;
-				_maxGramSet = true;
-			}
-		}
-
-		/// <summary>
-		/// token_chars Characters classes to keep in the tokens, Elasticsearch will split on characters that don’t belong to any of these classes.
-		/// [] (Keep all characters)
-		/// token_chars accepts the following character classes:
-		///   letter for example a, b, ï or 京
-		///   digit for example 3 or 7
-		///   whitespace for example " " or "\n"
-		///   punctuation for example ! or "
-		///   symbol for example $ or √ 
-		/// </summary>
-		public List<TokenChar> TokenChars
-		{
-			get { return _tokenChars; }
-			set
-			{
-				_tokenChars = value;
-				_tokenCharsSet = true;
-			}
-		}
-
-		public override void WriteJson(ElasticsearchCrudJsonWriter elasticsearchCrudJsonWriter)
-		{
-			base.WriteJsonBase(elasticsearchCrudJsonWriter, WriteValues);
-		}
-
-		private void WriteValues(ElasticsearchCrudJsonWriter elasticsearchCrudJsonWriter)
-		{
-			//"preserve_original" : true
-			JsonHelper.WriteValue("type", Type, elasticsearchCrudJsonWriter);
-			JsonHelper.WriteValue("min_gram", _minGram, elasticsearchCrudJsonWriter, _minGramSet);
-			JsonHelper.WriteValue("max_gram", _maxGram, elasticsearchCrudJsonWriter, _maxGramSet);
-			WriteTokenCharValue("token_chars", _tokenChars, elasticsearchCrudJsonWriter, _tokenCharsSet);
-		}
-
-		public static void WriteTokenCharValue(string key, List<TokenChar> valueObj, ElasticsearchCrudJsonWriter elasticsearchCrudJsonWriter, bool writeValue = true)
-		{
-			if (writeValue)
-			{
-				elasticsearchCrudJsonWriter.JsonWriter.WritePropertyName(key);
-				elasticsearchCrudJsonWriter.JsonWriter.WriteStartArray();
-
-				foreach (var obj in valueObj)
-				{
-					elasticsearchCrudJsonWriter.JsonWriter.WriteValue(obj.ToString());
-				}
-
-				elasticsearchCrudJsonWriter.JsonWriter.WriteEndArray();
-			}
 		}
 	}
 
---
+++ ElasticsearchCRUD/ContextAddDeleteUpdate/IndexModel/SettingsModel/Tokenizers/NGramTokenizer.cs
@@ -0,0 +1,18 @@
+﻿using ElasticsearchCRUD.Model;
+
+namespace ElasticsearchCRUD.ContextAddDeleteUpdate.IndexModel.SettingsModel.Tokenizers
+{
+	public class NGramTokenizer : BaseNGramTokenizer
+	{
+		/// <summary>
+		/// A tokenizer of type nGram.
+		/// </summary>
+		/// <param name="name">name of the custom tokenizer (ToLower()</param>
+		public NGramTokenizer(string name)
+		{
+			AnalyzerSet = true;
+			Name = name.ToLower();
+			Type = DefaultTokenizers.NGram;
+		}
+	}
+}
