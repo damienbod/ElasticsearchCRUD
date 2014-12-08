ee9c825c20cb82559261fe32614e884168b2ef05
damienbod <damien_bod@hotmail.com>
2014-12-08 19:06:36 +0100

    Add Shingle test

--- ElasticsearchCRUD.Integration.Test/AnalysisSettingsTests.cs
+++ ElasticsearchCRUD.Integration.Test/AnalysisSettingsTests.cs
@@ -274,6 +274,94 @@ namespace ElasticsearchCRUD.Integration.Test
 			Thread.Sleep(1500);
 			TearDown();
 		}
+
+		//
+		//		"settings" : {
+		//   "analysis" : {
+		//	   "filter" : {
+		//		   "blocks_filter" : {
+		//			   "type" : "word_delimiter",
+		//			   "preserve_original": "true"
+		//		   },
+		//		  "shingle":{
+		//			  "type":"shingle",
+		//			  "max_shingle_size":5,
+		//			  "min_shingle_size":2,
+		//			  "output_unigrams":"true"
+		//		   },
+		//		   "filter_stop":{
+		//			  "type":"stop",
+		//			  "ignore_case":"true"
+		//		   }
+		//	   },
+		//	   "analyzer" : {
+		//		   "blocks_analyzer" : {
+		//			   "type" : "custom",
+		//			   "tokenizer" : "whitespace",
+		//			   "filter" : ["lowercase", "blocks_filter", "shingle"]
+		//		   }
+		//	   }
+		//   }
+		//},
+		[Test]
+		public void CreateNewIndexDefinitionShingle()
+		{
+			var testSettingsIndexDefinition = new IndexDefinition
+			{
+				IndexSettings =
+				{
+					Analysis = new Analysis
+					{
+						Analyzer =
+						{
+							Analyzers = new List<AnalyzerBase>
+							{
+								new CustomAnalyzer("blocks_analyzer")
+								{
+									Tokenizer= DefaultTokenizers.Whitespace,
+									Filter = new List<string>{DefaultTokenFilters.Lowercase, "blocks_filter", "shingle"}
+								}
+							}
+						},
+						Filters =
+						{
+							CustomFilters = new List<AnalysisFilterBase>
+							{
+								new WordDelimiterTokenFilter("blocks_filter")
+								{
+									PreserveOriginal = true
+								},
+								new ShingleTokenFilter("shingle")
+								{
+									MaxShingleSize = 5,
+									MinShingleSize = 2,
+									OutputUnigrams = true
+								},
+								new StopTokenFilter("filter_stop")
+								{
+									IgnoreCase = true
+								}
+							}
+						}
+					},
+					NumberOfShards = 2,
+					NumberOfReplicas = 1
+				},
+			};
+
+			using (var context = new ElasticsearchContext(ConnectionString, new ElasticsearchSerializerConfiguration(_elasticsearchMappingResolver)))
+			{
+				context.TraceProvider = new ConsoleTraceProvider();
+				context.IndexCreate<TestSettingsIndex>(testSettingsIndexDefinition);
+
+				Thread.Sleep(1500);
+				Assert.IsTrue(context.IndexExists<TestSettingsIndex>());
+			}
+
+			Thread.Sleep(1500);
+			TearDown();
+		}
+
 	}
 
 	public class TestSettingsIndex
