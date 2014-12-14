f0f069a9ce382daecff2e14f093359efcda7994b
damienbod <damien_bod@hotmail.com>
2014-12-14 17:54:12 +0100

    default similarities

--- ElasticsearchCRUD/Model/DefaultSimilarities.cs
+++ ElasticsearchCRUD/Model/DefaultSimilarities.cs
@@ -4,5 +4,9 @@
 	{
 		public const string Default = "default";
 		public const string Bm25 = "BM25";
+		public const string Dfr = "DFR";
+		public const string Ib = "IB";
+		public const string LmDirichlet = "LMDirichlet";
+		public const string LmJelinekMercer = "LMJelinekMercer";
 	}
 }
