d9a3ec5dfc723191f251a89f2900684911970d6e
damienbod <damien_bod@hotmail.com>
2015-01-28 08:17:10 +0100

    test clean up

--- ElasticsearchCRUD.Integration.Test/SearchTests/SetupSearch.cs
+++ ElasticsearchCRUD.Integration.Test/SearchTests/SetupSearch.cs
@@ -8,7 +8,7 @@ namespace ElasticsearchCRUD.Integration.Test.SearchTests
 	public class SetupSearch
 	{
 		protected readonly IElasticsearchMappingResolver ElasticsearchMappingResolver = new ElasticsearchMappingResolver();
-		protected const string ConnectionString = "http://localhost.fiddler:9200";
+		protected const string ConnectionString = "http://localhost:9200";
 
 		[TestFixtureSetUp]
 		public void Setup()
