6666f48e48448c081a22632d57b0b3d2ebd70797
damienbod <damien_bod@hotmail.com>
2015-02-07 10:03:44 +0100

    release 1.0.27

--- ElasticsearchCRUD/Properties/AssemblyInfo.cs
+++ ElasticsearchCRUD/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Damienbod.ElasticSearchProvider")]
+[assembly: AssemblyTitle("Damienbod.ElasticsearchCRUD")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ElasticSearchProvider")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("ElasticsearchCRUD")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.26")]
-[assembly: AssemblyFileVersion("1.0.26")]
+[assembly: AssemblyVersion("1.0.27")]
+[assembly: AssemblyFileVersion("1.0.27")]
