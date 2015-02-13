e90809c1203279ee6fd1ee7bb73261dc036606d3
damienbod <damien_bod@hotmail.com>
2015-02-13 09:47:23 +0100

    Warmer PUT

---
+++ ElasticsearchCRUD/ContextWarmers/ElasticsearchContextWarmer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using ElasticsearchCRUD.Model;
+using ElasticsearchCRUD.Tracing;
+using ElasticsearchCRUD.Utils;
+
+namespace ElasticsearchCRUD.ContextWarmers
+{
+	class ElasticsearchContextWarmer
+	{
+		private readonly ITraceProvider _traceProvider;
+		private readonly CancellationTokenSource _cancellationTokenSource;
+		private readonly ElasticsearchSerializerConfiguration _elasticsearchSerializerConfiguration;
+		private readonly HttpClient _client;
+		private readonly string _connectionString;
+
+		public ElasticsearchContextWarmer(ITraceProvider traceProvider, CancellationTokenSource cancellationTokenSource, ElasticsearchSerializerConfiguration elasticsearchSerializerConfiguration, HttpClient client, string connectionString)
+		{
+			_traceProvider = traceProvider;
+			_cancellationTokenSource = cancellationTokenSource;
+			_elasticsearchSerializerConfiguration = elasticsearchSerializerConfiguration;
+			_client = client;
+			_connectionString = connectionString;
+		}
+
+		public bool SendWarmerCommand(string contentJson, string index, string type, string warmerName)
+		{
+			var syncExecutor = new SyncExecute(_traceProvider);
+			return syncExecutor.Execute(() => SendWarmerCommandAsync(contentJson, index, type, warmerName));	
+		}
+
+		public async Task<ResultDetails<bool>> SendWarmerCommandAsync(string contentJson, string index, string type, string warmerName)
+		{
+			_traceProvider.Trace(TraceEventType.Verbose, string.Format("ElasticsearchContextWarmer: Creating Warmer {0}", contentJson));
+
+			var resultDetails = new ResultDetails<bool> { Status = HttpStatusCode.InternalServerError };
+			var elasticsearchUrl = string.Format("{0}/_warmer/{1}/{2}/{3}", _connectionString, index, type, warmerName);
+			var uri = new Uri(elasticsearchUrl);
+			_traceProvider.Trace(TraceEventType.Verbose, "{1}: Request HTTP PUT uri: {0}", uri.AbsoluteUri, "ElasticsearchContextWarmer");
+
+			var content = new StringContent(contentJson);
+			var response = await _client.PutAsync(uri, content, _cancellationTokenSource.Token).ConfigureAwait(false);
+
+			if (response.StatusCode == HttpStatusCode.OK)
+			{
+				resultDetails.PayloadResult = true;
+				return resultDetails;
+			}
+
+			_traceProvider.Trace(TraceEventType.Error, string.Format("ElasticsearchContextWarmer: Cound Not Execute Alias {0}", contentJson));
+			throw new ElasticsearchCrudException(string.Format("ElasticsearchContextWarmer: Cound Not Execute Alias  {0}", contentJson));
+		}
+	}
+}
---
+++ ElasticsearchCRUD/ContextWarmers/Warmer.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using ElasticsearchCRUD.Model.SearchModel;
+
+namespace ElasticsearchCRUD.ContextWarmers
+{
+	public class Warmer
+	{
+		private IQueryHolder _query;
+		private bool _querySet;
+		private List<IAggs> _aggs;
+		private bool _aggsSet;
+
+		public IQueryHolder Query
+		{
+			get { return _query; }
+			set
+			{
+				_query = value;
+				_querySet = true;
+			}
+		}
+
+		/// <summary>
+		/// aggregations request
+		/// </summary>
+		public List<IAggs> Aggs
+		{
+			get { return _aggs; }
+			set
+			{
+				_aggs = value;
+				_aggsSet = true;
+			}
+		}
+
+		public void WriteJson(ElasticsearchCrudJsonWriter elasticsearchCrudJsonWriter)
+		{
+			elasticsearchCrudJsonWriter.JsonWriter.WriteStartObject();
+
+			if (_querySet)
+			{
+				_query.WriteJson(elasticsearchCrudJsonWriter);
+			}
+
+			if (_aggsSet)
+			{
+				elasticsearchCrudJsonWriter.JsonWriter.WritePropertyName("aggs");
+				elasticsearchCrudJsonWriter.JsonWriter.WriteStartObject();
+				foreach (var item in _aggs)
+				{
+					item.WriteJson(elasticsearchCrudJsonWriter);
+				}
+
+				elasticsearchCrudJsonWriter.JsonWriter.WriteEndObject();
+			}
+
+			elasticsearchCrudJsonWriter.JsonWriter.WriteEndObject();
+		}
+	}
+}
