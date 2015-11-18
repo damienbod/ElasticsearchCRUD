1511e8f9fc648f3317fce08468d9a7d61b3cefe6
damienbod <damien_bod@hotmail.com>
2015-11-18 22:51:32 +0100

    bug fixing in search model

--- ElasticsearchCrudAspNet5/src/ElasticsearchCrud/Model/SearchModel/Filters/TermFilter.cs
+++ ElasticsearchCrudAspNet5/src/ElasticsearchCrud/Model/SearchModel/Filters/TermFilter.cs
@@ -6,23 +6,11 @@ namespace ElasticsearchCRUD.Model.SearchModel.Filters
 	{
 		private readonly string _term;
 		private readonly string _termValue;
-		private bool _cache;
-		private bool _cacheSet;
 
 		public TermFilter(string term, string termValue)
 		{
 			_term = term;
 			_termValue = termValue;
-		}
-
-		public bool Cache
-		{
-			get { return _cache; }
-			set
-			{
-				_cache = value;
-				_cacheSet = true;
-			}
 		}
 
 		public void WriteJson(ElasticsearchCrudJsonWriter elasticsearchCrudJsonWriter)
@@ -31,7 +19,6 @@ namespace ElasticsearchCRUD.Model.SearchModel.Filters
 			elasticsearchCrudJsonWriter.JsonWriter.WriteStartObject();
 
 			JsonHelper.WriteValue(_term, _termValue, elasticsearchCrudJsonWriter);
-			JsonHelper.WriteValue("_cache", _cache, elasticsearchCrudJsonWriter, _cacheSet);
 
 			elasticsearchCrudJsonWriter.JsonWriter.WriteEndObject();
 		}
