dfc30309d640edddd568a5165e54ed8d5b707d7e
damienbod <damien_bod@hotmail.com>
2015-11-20 09:43:18 +0100

    template for integration tests ASP.NET 5 using xUnit

---
+++ ElasticsearchCrudAspNet5/src/ElasticsearchCRUD.Integration.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ElasticsearchCRUD.Integration.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ElasticsearchCRUD.Integration.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6467b9af-6eaf-4be5-8eaf-b0a7dd8f3371")]
