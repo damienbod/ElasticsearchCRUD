eb62211f15546b6cf73280889a54ebca7a26b8c1
damienbod <damien_bod@hotmail.com>
2016-07-05 06:41:36 +0200

    indexer test

--- samples/DataTransferEF_SQLToElasticsearch/Program.cs
+++ samples/DataTransferEF_SQLToElasticsearch/Program.cs
@@ -3,24 +3,24 @@ using DataTransferSQLToEl.ExampleReindexChildDocuments;
 
 namespace DataTransferSQLToEl
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			Repo repo = new Repo();
-			//repo.SaveToElasticsearchPerson();
-			
-			//repo.SaveToElasticsearchStateProvince();
-			var addressX = repo.GetAddressFromElasticsearch("37", "14");
-			Console.WriteLine(addressX);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Repo repo = new Repo();
+            //repo.SaveToElasticsearchPerson();
+            
+            //repo.SaveToElasticsearchStateProvince();
+            var addressX = repo.GetAddressFromElasticsearch("37", "14");
+            Console.WriteLine(addressX);
 
-			//DateTime beginDateTime = DateTime.UtcNow;
+            DateTime beginDateTime = DateTime.UtcNow;
 
-			//Reindexer.ReindexStateProvince(beginDateTime);
-			//Reindexer.ReindexStateProvinceAddress(beginDateTime);
+            Reindexer.ReindexStateProvince(beginDateTime);
+            Reindexer.ReindexStateProvinceAddress(beginDateTime);
 
-			//Console.WriteLine("Created new index from version 1 index");
-			Console.ReadLine();
-		}
-	}
+            Console.WriteLine("Created new index from version 1 index");
+            Console.ReadLine();
+        }
+    }
 }
